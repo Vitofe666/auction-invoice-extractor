cd14661bc18b24f0528d19bdd6ef0dcf27025d79
Vitofe666 <vferrone@gmail.com>
2025-11-04 13:04:35 +0100

    Fix Xero file attachment: Add accounting.attachments scope to OAuth

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ const xero = new XeroClient({
    clientId: process.env.XERO_CLIENT_ID,
    clientSecret: process.env.XERO_CLIENT_SECRET,
    redirectUris: [process.env.XERO_REDIRECT_URI],
-   scopes: 'openid profile email accounting.transactions accounting.contacts offline_access'.split(' ')
+   scopes: 'openid profile email accounting.transactions accounting.contacts accounting.attachments offline_access'.split(' ')
 });
 
 console.log('✓ Xero client initialized');
